74d49c94d10c93102d43a81f89b6f56e13ef4378
Michael Smith <michasmi@adobe.com>
2025-07-30 23:20:27 -0500

    Updates to main page

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -10,7 +10,23 @@ export default function decorate(block) {
     while (row.firstElementChild) li.append(row.firstElementChild);
     [...li.children].forEach((div) => {
       if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else div.className = 'cards-card-body';
+      else {
+        div.className = 'cards-card-body';
+
+        const cardtext = div.textContent.split(/\r?\n/);
+        const titletext = document.createElement('h3');
+        titletext.className = 'card-title';
+        titletext.innerHTML = cardtext[1];
+
+         const linktext = document.createElement('p');
+         linktext.className = 'card-link-text';
+         linktext.innerHTML = cardtext[2];
+
+
+         div.innerHTML = '';
+         div.append(titletext);
+         div.append(linktext);
+      }
     });
     ul.append(li);
   });
--- blocks/cta-banner/cta-banner.js
+++ blocks/cta-banner/cta-banner.js
@@ -1,10 +1,17 @@
 export default async function decorate(block) {
-  const [ctalink, attribution] = [...block.children].map((c) => c.firstElementChild);
+  const [ctalinktext, attribution] = [...block.children].map((c) => c.firstElementChild);
   const ctabanner = document.createElement('div');
+  const ctatext = ctalinktext.textContent.split(/\r?\n/);
+  const ctalink = document.createElement('h2');
+  ctalink.innerHTML = ctatext[1];
+  const sublink = document.createElement('p');
+  sublink.innerHTML = ctatext[2];
   // decorate cta-banner
   ctabanner.className = 'ctabanner';
   ctalink.className = 'ctalink';
+  sublink.className = 'sublink';
   ctabanner.append(ctalink);
+  ctabanner.append(sublink);
  
   block.innerHTML = '';
   block.append(ctabanner);
